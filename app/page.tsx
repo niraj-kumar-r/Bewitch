580101ed02672f4244c5e496d572dd2accef514c
Niraj Kumar <nirajk23523@gmail.com>
2023-12-13 21:43:55 +0530

    Dev : added auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
-    return <Button>Click me</Button>;
+    return <UserButton afterSignOutUrl="/" />;
 }
