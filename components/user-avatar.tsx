73a170c7fae6ae8be1976b430abc456ef85abacd
Niraj Kumar <nirajk23523@gmail.com>
2024-02-07 22:24:09 +0530

    Dev : Check

---
+++ components/user-avatar.tsx
@@ -0,0 +1,9 @@
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export const UserAvatar = ({ imageUrl, username, isLive }) => {
+    return <div>User Avatar</div>;
+};
