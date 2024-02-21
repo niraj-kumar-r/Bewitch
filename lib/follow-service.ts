27a9f9949255aeda04b9030faa07b2ab38d7fed6
Niraj Kumar <nirajk23523@gmail.com>
2024-02-21 23:57:00 +0530

    Dev : Follow service done

--- lib/follow-service.ts
+++ lib/follow-service.ts
@@ -1,6 +1,25 @@
 import { getSelf } from "./auth-service";
 import { db } from "./db";
 
+export const getFollowedUsers = async () => {
+    try {
+        const self = await getSelf();
+
+        const followedUsers = await db.follow.findMany({
+            where: {
+                followerId: self.id,
+            },
+            include: {
+                following: true,
+            },
+        });
+
+        return followedUsers;
+    } catch {
+        return [];
+    }
+};
+
 export const isFollowingUser = async (id: string) => {
     try {
         const self = await getSelf();
