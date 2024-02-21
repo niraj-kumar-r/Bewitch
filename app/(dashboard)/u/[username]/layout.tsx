ff82c2a2fc7b670deede3fecd70d4d32be64264a
Niraj Kumar <nirajk23523@gmail.com>
2024-02-22 01:12:08 +0530

    Dev : started Sidebar

--- app/(dashboard)/u/[username]/layout.tsx
+++ app/(dashboard)/u/[username]/layout.tsx
@@ -1,6 +1,7 @@
 import { getSelfByUsername } from "@/lib/auth-service";
 import { redirect } from "next/navigation";
 import { Navbar } from "./_components/navbar";
+import Sidebar from "./_components/sidebar";
 
 interface CreatorLayoutProps {
     params: {
@@ -19,7 +20,10 @@ const CreatorLayout = async ({ params, children }: CreatorLayoutProps) => {
     return (
         <>
             <Navbar />
-            <div className="flex h-full pt-20">{children}</div>
+            <div className="flex h-full pt-20">
+                <Sidebar />
+                {children}
+            </div>
         </>
     );
 };
