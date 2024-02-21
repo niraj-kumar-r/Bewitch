7027a26073b1a329cf7d34228723e167c5d6d2f8
Niraj Kumar <nirajk23523@gmail.com>
2024-02-22 01:04:06 +0530

    Dev : Creator Navbar done

---
+++ app/(dashboard)/u/[username]/_components/navbar/actions.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+
+export const Actions = () => {
+    return (
+        <div className="flex items-center justify-end gap-x-2">
+            <Button
+                size="sm"
+                variant="ghost"
+                asChild
+                className="text-muted-foreground hover:text-primary"
+            >
+                <Link href="/">
+                    <LogOut className="h-5 w-5 mr-2" />
+                    Exit
+                </Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    );
+};
