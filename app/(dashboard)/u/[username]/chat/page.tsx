a689b8f8770b62bccb462575f96ffbef530a80a0
Niraj Kumar <nirajk23523@gmail.com>
2024-02-23 15:26:30 +0530

    Dev : made Stream model

---
+++ app/(dashboard)/u/[username]/chat/page.tsx
@@ -0,0 +1,11 @@
+const ChatPage = () => {
+    return (
+        <div className="p-6">
+            <div className=" mb-4">
+                <h1 className="text-2xl font-bold">Chat settings</h1>
+            </div>
+        </div>
+    );
+};
+
+export default ChatPage;
