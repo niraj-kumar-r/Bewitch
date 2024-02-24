b44fad2e7de3e82995d76a64f16499501bb44cc7
Niraj Kumar <nirajk23523@gmail.com>
2024-02-24 19:50:58 +0530

    Bugfix 2

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { useSidebar } from "@/store/use-sidebar";
-import { Follow, Stream, User } from "@prisma/client";
-import { stat } from "fs";
+import { Follow, User } from "@prisma/client";
 import { UserItem, UserItemSkeleton } from "./user-item";
 
 interface FollowingProps {
-    data: (Follow & { following: User & { stream: Stream | null } })[];
+    data: (Follow & {
+        following: User & { stream: { isLive: boolean } | null };
+    })[];
 }
 
 export const Following = ({ data }: FollowingProps) => {
