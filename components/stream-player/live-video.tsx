dfedae77cdb33ae28ae25e6bdb6ea1e830b76b51
Niraj Kumar <nirajk23523@gmail.com>
2024-02-25 01:43:24 +0530

    Dev : Added fullscreen control overlay

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -3,6 +3,7 @@
 import { useRef } from "react";
 import { Participant, Track } from "livekit-client";
 import { useTracks, UseTracksOptions } from "@livekit/components-react";
+import { FullscreenControl } from "./fullscreen-control";
 
 interface LiveVideoProps {
     participant: Participant;
@@ -23,6 +24,14 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
     return (
         <div ref={wrapperRef} className="relative h-full flex">
             <video ref={videoRef} width="100%" />
+            <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
+                <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+                    <FullscreenControl
+                        isFullscreen={false}
+                        onToggle={() => {}}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
