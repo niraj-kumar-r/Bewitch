06ff954d8c95311a67d207ccdd5ae7b36b1e7382
Niraj Kumar <nirajk23523@gmail.com>
2024-02-24 19:28:58 +0530

    Dev : added isLive from db

--- app/(browse)/_components/sidebar/following.tsx
+++ app/(browse)/_components/sidebar/following.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import { useSidebar } from "@/store/use-sidebar";
-import { Follow, User } from "@prisma/client";
+import { Follow, Stream, User } from "@prisma/client";
 import { stat } from "fs";
 import { UserItem, UserItemSkeleton } from "./user-item";
 
 interface FollowingProps {
-    data: (Follow & { following: User })[];
+    data: (Follow & { following: User & { stream: Stream | null } })[];
 }
 
 export const Following = ({ data }: FollowingProps) => {
@@ -28,6 +28,7 @@ export const Following = ({ data }: FollowingProps) => {
                         key={follow.following.id}
                         username={follow.following.username}
                         imageUrl={follow.following.imageUrl}
+                        isLive={follow.following.stream?.isLive}
                     />
                 ))}
             </ul>
