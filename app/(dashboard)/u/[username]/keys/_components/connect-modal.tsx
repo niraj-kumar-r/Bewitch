df57dc1ba8fa802b938d99a785364156bebc4007
Niraj Kumar <nirajk23523@gmail.com>
2024-02-24 18:21:38 +0530

    Dev : added ingress server actions

--- app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
+++ app/(dashboard)/u/[username]/keys/_components/connect-modal.tsx
@@ -9,6 +9,13 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
 
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { AlertTriangle } from "lucide-react";
@@ -23,6 +30,15 @@ export const ConnectModal = () => {
                 <DialogHeader>
                     <DialogTitle>Generate Connection</DialogTitle>
                 </DialogHeader>
+                <Select>
+                    <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Ingress Type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="RTMP">RTMP</SelectItem>
+                        <SelectItem value="WHIP">WHIP</SelectItem>
+                    </SelectContent>
+                </Select>
                 <Alert>
                     <AlertTriangle className="h-4 w-4" />
                     <AlertTitle>Warning!</AlertTitle>
@@ -31,6 +47,14 @@ export const ConnectModal = () => {
                         current connection
                     </AlertDescription>
                 </Alert>
+                <div className="flex justify-between">
+                    <DialogClose>
+                        <Button variant="ghost">Cancel</Button>
+                    </DialogClose>
+                    <Button variant="primary" onClick={() => {}}>
+                        Generate
+                    </Button>
+                </div>
             </DialogContent>
         </Dialog>
     );
