83d962d863c2542602ccaaa8d3976c2ee8d4ca59
Niraj Kumar <nirajk23523@gmail.com>
2024-02-29 00:17:07 +0530

    Dev : Volume control works

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -1,13 +1,18 @@
 "use client";
 
 import { LiveKitRoom } from "@livekit/components-react";
-import { useViewerToken } from "@/hooks/use-viewer-token";
 import { Stream, User } from "@prisma/client";
+
+import { cn } from "@/lib/utils";
+import { useChatSidebar } from "@/store/use-chat-sidebar";
+import { useViewerToken } from "@/hooks/use-viewer-token";
+
+import { Chat } from "./chat";
 import { Video } from "./video";
 
 interface StreamPlayerProps {
     user: User & { stream: Stream | null };
-    stream: Stream | null;
+    stream: Stream;
     isFollowing: boolean;
 }
 
@@ -17,6 +22,7 @@ export const StreamPlayer = ({
     isFollowing,
 }: StreamPlayerProps) => {
     const { token, name, identity } = useViewerToken(user.id);
+    const { collapsed } = useChatSidebar((state) => state);
 
     if (!token || !name || !identity) {
         return <div>Cannot watch stream</div>;
@@ -27,10 +33,24 @@ export const StreamPlayer = ({
             <LiveKitRoom
                 token={token}
                 serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL}
-                className="grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full"
+                className={cn(
+                    "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+                    collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
+                )}
             >
                 <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
                     <Video hostname={user.username} hostIdentity={user.id} />
+                </div>
+                <div className={cn("col-span-1", collapsed && "hidden")}>
+                    <Chat
+                        viewerName={name}
+                        hostName={user.username}
+                        hostIdentity={user.id}
+                        isFollowing={isFollowing}
+                        isChatEnabled={stream.isChatEnabled}
+                        isChatDelayed={stream.isChatDelayed}
+                        isChatFollowersOnly={stream.isChatFollowersOnly}
+                    />
                 </div>
             </LiveKitRoom>
         </>
--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -1,9 +1,11 @@
 "use client";
 
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Participant, Track } from "livekit-client";
 import { useTracks, UseTracksOptions } from "@livekit/components-react";
+import { useEventListener } from "usehooks-ts";
 import { FullscreenControl } from "./fullscreen-control";
+import { VolumeControl } from "./volume-control";
 
 interface LiveVideoProps {
     participant: Participant;
@@ -12,6 +14,47 @@ interface LiveVideoProps {
 export const LiveVideo = ({ participant }: LiveVideoProps) => {
     const videoRef = useRef<HTMLVideoElement>(null);
     const wrapperRef = useRef<HTMLDivElement>(null);
+
+    const [isFullscreen, setIsFullscreen] = useState(false);
+    const [volume, setVolume] = useState(0);
+
+    const onVolumeChange = (value: number) => {
+        setVolume(+value);
+        if (videoRef?.current) {
+            videoRef.current.muted = value === 0;
+            videoRef.current.volume = +value * 0.01;
+        }
+    };
+
+    const toggleMute = () => {
+        const isMuted = volume === 0;
+
+        setVolume(isMuted ? 50 : 0);
+
+        if (videoRef?.current) {
+            videoRef.current.muted = !isMuted;
+            videoRef.current.volume = isMuted ? 0.5 : 0;
+        }
+    };
+
+    useEffect(() => {
+        onVolumeChange(0);
+    }, []);
+
+    const toggleFullscreen = () => {
+        if (isFullscreen) {
+            document.exitFullscreen();
+        } else if (wrapperRef?.current) {
+            wrapperRef.current.requestFullscreen();
+        }
+    };
+
+    const handleFullscreenChange = () => {
+        const isCurrentlyFullscreen = document.fullscreenElement !== null;
+        setIsFullscreen(isCurrentlyFullscreen);
+    };
+
+    useEventListener("fullscreenchange", handleFullscreenChange, wrapperRef);
 
     useTracks([Track.Source.Camera, Track.Source.Microphone])
         .filter((track) => track.participant.identity === participant.identity)
@@ -26,9 +69,14 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
             <video ref={videoRef} width="100%" />
             <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
                 <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
+                    <VolumeControl
+                        onChange={onVolumeChange}
+                        onToggle={toggleMute}
+                        value={volume}
+                    />
                     <FullscreenControl
-                        isFullscreen={false}
-                        onToggle={() => {}}
+                        isFullscreen={isFullscreen}
+                        onToggle={toggleFullscreen}
                     />
                 </div>
             </div>
